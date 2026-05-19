fa4334045c4248dfb47aaf34df92cb0cf18093f8
Utsav Raj <utsavrajdocx@gmail.com>
2026-05-19 19:49:34 +0530

    Two Sum

--- 2026_Q130/TwoSum.cpp
+++ 2026_Q130/TwoSum.cpp
@@ -3,8 +3,20 @@
 #include <unordered_map>
 using namespace std;
 
-vector<int>towSum(vector<int>&nums,int target){
-    
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> mp;
+
+    for (int i = 0; i < nums.size(); i++) {
+        int complement = target - nums[i];
+
+        if (mp.find(complement) != mp.end()) {
+            return {mp[complement], i};
+        }
+
+        mp[nums[i]] = i;
+    }
+
+    return {};
 }
 
 int main() {
