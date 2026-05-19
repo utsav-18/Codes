7b8617ba3a366fcfe2bf50a410172661daa923f9
Utsav Raj <utsavrajdocx@gmail.com>
2026-05-19 19:18:21 +0530

    New Era!

---
+++ 2026_Q130/TwoSum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+vector<int>towSum(vector<int>&nums,int target){
+    
+}
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    vector<int> ans = twoSum(nums, target);
+
+    cout << ans[0] << " " << ans[1];
+
+    return 0;
+}
