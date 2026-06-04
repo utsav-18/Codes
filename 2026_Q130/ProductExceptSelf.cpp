57d8214f2a0ba559f99a800299b7fa507cd2cfbd
Utsav Raj <utsavrajdocx@gmail.com>
2026-06-04 19:34:33 +0530

    Leet

--- 2026_Q130/ProductExceptSelf.cpp
+++ 2026_Q130/ProductExceptSelf.cpp
@@ -1,2 +1,24 @@
 #include<iostream>
+#include <vector>
 using namespace std;
+
+vector<int> productExceptSelf(vector<int>& nums) {
+    vector<int>ans;
+
+}
+
+void print(vector<int>& vec){
+    for(int i=0;i<vec.size();i++){
+        cout<<vec[i]<<" ";
+    }
+        cout<<endl;
+}
+
+int main(){
+    vector<int>v;
+    v={1,2,3,4};
+    print(v);
+    vector<int>pro;
+    pro = productExceptSelf(v);
+    print(pro);
+}
