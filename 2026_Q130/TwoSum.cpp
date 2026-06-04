fc83257f3732c8187c593a93625c53571f4c9a5a
Utsav Raj <utsavrajdocx@gmail.com>
2026-06-04 19:42:43 +0530

    Leet

--- 2026_Q130/TwoSum.cpp
+++ 2026_Q130/TwoSum.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include <unordered_map>
 using namespace std;
-
 vector<int> twoSum(vector<int>& nums,int target){
     unordered_map<int,int>m;
     for(int i=0;i<nums.size();i++){
@@ -14,14 +13,10 @@ vector<int> twoSum(vector<int>& nums,int target){
     }
     return {};
 }
-
 int main() {
     vector<int> nums = {0, 7, 11, 15,1,2,3,4,5};
     int target = 9;
-
     vector<int> ans = twoSum(nums, target);
-
     cout << ans[0] << " " << ans[1];
-
     return 0;
 }
