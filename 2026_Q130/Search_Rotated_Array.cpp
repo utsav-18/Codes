e808d2c015ea0b413602c60161554262f0e9fdb9
Utsav Raj <utsavrajdocx@gmail.com>
2026-06-06 12:16:43 +0530

    Leet

---
+++ 2026_Q130/Search_Rotated_Array.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] == target)
+                return mid;
+
+            // Left half sorted
+            if(nums[left] <= nums[mid]) {
+                if(target >= nums[left] && target < nums[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }
+            // Right half sorted
+            else {
+                if(target > nums[mid] && target <= nums[right])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    int main(){
+
+    }
