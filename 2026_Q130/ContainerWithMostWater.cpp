ba1ec230e29e71a12943ebb2d79af1502eefe10a
Utsav Raj <utsavrajdocx@gmail.com>
2026-06-08 18:25:08 +0530

    Leet

---
+++ 2026_Q130/ContainerWithMostWater.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+    int maxArea(vector<int>& height) {
+        
+    }
+
+int main(){
+    vector<int> height ;
+    height  = {1,8,6,2,5,4,8,3,7};
+    cout<<maxArea(height);
+}
