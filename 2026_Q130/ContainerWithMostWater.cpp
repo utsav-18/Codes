293da418fc1401b661b4b13018827f5bfef453ba
Utsav Raj <utsavrajdocx@gmail.com>
2026-06-08 18:44:06 +0530

    Leet

--- 2026_Q130/ContainerWithMostWater.cpp
+++ 2026_Q130/ContainerWithMostWater.cpp
@@ -4,7 +4,21 @@
 using namespace std;
 
     int maxArea(vector<int>& height) {
-        
+        int maxA=0;
+        int left = 0;
+        int right = height.size()-1;
+        while(left<right){
+            int w = right-left;
+            int area = min(height[left],height[right])*w; //min h because water can be stored until the shorter wall only!
+            maxA = max(maxA,area);
+            if(height[left]<height[right]){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return maxA;
     }
 
 int main(){
