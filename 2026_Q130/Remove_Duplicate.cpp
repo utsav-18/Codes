df2b16d4b0ca85e67e7f0728e456b40331487694
Utsav Raj <utsavrajdocx@gmail.com>
2026-06-08 18:14:25 +0530

    Leet

--- 2026_Q130/Remove_Duplicate.cpp
+++ 2026_Q130/Remove_Duplicate.cpp
@@ -1,21 +1,31 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-    int removeDuplicates(vector<int>& nums) {
-        int count=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==nums[i+1]){
-                count++;
-            }
+int removeDuplicates(vector<int>& nums) {
+    if(nums.size() == 0) return 0;
+
+    int i = 0;
+    for(int j = 1; j < nums.size(); j++) {
+        if(nums[i] != nums[j]) {
+            i++;
+            nums[i] = nums[j];
         }
-        return count;
+    }
+    return i + 1;
+}
+
+int main() {
+    vector<int> nums = {0,0,1,1,1,2,2,3,3,4};
+
+    int k = removeDuplicates(nums);
+
+    cout << "Length = " << k << endl;
+    cout << "Array: ";
+
+    for(int i = 0; i < k; i++) {
+        cout << nums[i] << " ";
     }
 
-int main(){
-    vector<int> nums;
-    nums = {0,0,1,1,1,2,2,3,3,4};
-    cout<<removeDuplicates(nums);
     return 0;
 }
