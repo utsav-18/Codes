0c08fa2b8e7afad08a6230349d80dfa3f45c3063
Utsav Raj <utsavrajdocx@gmail.com>
2026-06-10 09:41:54 +0530

    Leet

--- 2026_Q130/Remove_Duplicate.cpp
+++ 2026_Q130/Remove_Duplicate.cpp
@@ -16,7 +16,7 @@ int removeDuplicates(vector<int>& nums) {
 }
 
 int main() {
-    vector<int> nums = {0,0,1,1,1,2,2,3,3,4};
+    vector<int> nums = {0,0,1,1,1,2,2,3,3,4,5,5,6,7,8,9};
 
     int k = removeDuplicates(nums);
 
