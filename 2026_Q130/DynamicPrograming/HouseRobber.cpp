eb013fedb99e44b32b19f91f47dcb4f10f03554b
Utsav Raj <utsavrajdocx@gmail.com>
2026-06-24 20:06:07 +0530

    DP

---
+++ 2026_Q130/DynamicPrograming/HouseRobber.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+    int rob(vector<int>& nums) {
+
+        int rob1 = 0;
+        int rob2 = 0;
+        int maxi = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            maxi = max(rob1 + nums[i], rob2);
+
+            rob1 = rob2;
+            rob2 = maxi;
+        }
+
+        return maxi;
+    }
+
+    int main(){
+        vector<int>nums;
+        nums={1,2,3,1};
+        cout<<rob(nums);
+    }
