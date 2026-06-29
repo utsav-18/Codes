7cec5d8f550b87c666ad0e32a55c90b312540ad0
Utsav Raj <utsavrajdocx@gmail.com>
2026-06-29 09:14:24 +0530

    DP

---
+++ 2026_Q130/DynamicPrograming/Decodeways.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+    int numDecodings(string s) {
+
+        if (s[0] == '0')
+            return 0;
+
+        int one = 1;
+        int two = 1;
+
+
+int main(){
+
+}
