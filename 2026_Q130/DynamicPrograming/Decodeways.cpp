74f679805223c88d75a9f832abd469cacb9a2608
Utsav Raj <utsavrajdocx@gmail.com>
2026-06-29 09:15:12 +0530

    DP

--- 2026_Q130/DynamicPrograming/Decodeways.cpp
+++ 2026_Q130/DynamicPrograming/Decodeways.cpp
@@ -3,13 +3,33 @@
 #include <vector>
 using namespace std;
 
-    int numDecodings(string s) {
+        int numDecodings(string s) {
 
-        if (s[0] == '0')
-            return 0;
+            if (s[0] == '0')
+                return 0;
 
-        int one = 1;
-        int two = 1;
+            int one = 1;
+            int two = 1;
+        for (int i = 1; i < s.length(); i++) {
+
+                    int current = 0;
+
+                    // Decode single digit
+                    if (s[i] != '0')
+                        current = one;
+
+                    // Decode two digits
+                    int value = (s[i - 1] - '0') * 10 + (s[i] - '0');
+
+                    if (value >= 10 && value <= 26)
+                        current += two;
+
+                    two = one;
+                    one = current;
+                }
+
+                return one;
+        }
 
 
 int main(){
