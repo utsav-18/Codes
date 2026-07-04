ee377793450d84032249fbe8b07d776c1948ef71
Utsav Raj <utsavrajdocx@gmail.com>
2026-07-04 13:03:25 +0530

    Leet75

---
+++ 2026_Q130/DynamicPrograming/racecar.cpp
@@ -0,0 +1,34 @@
+
+
+    int racecar(int target) {
+        vector<int> dp(target + 1, -1);
+        dp[0] = 0;
+        return raceCar(target, dp);
+    }
+
+    int raceCar(int target, vector<int>& dp) {
+        if (dp[target] >= 0) {
+            return dp[target];
+        }
+
+        dp[target] = INT_MAX;
+
+        int x = 1, j = 1;
+
+        for (; j < target; j = (1 << ++x) - 1) {
+
+            int q = 0, p = 0;
+
+            while (p < j) {
+                dp[target] = min(dp[target],
+                                 x + 2 + q + raceCar(target - (j - p), dp));
+
+                p = (1 << ++q) - 1;
+            }
+        }
+
+        dp[target] = min(dp[target],
+                         x + (target == j ? 0 : 1 + raceCar(j - target, dp)));
+
+        return dp[target];
+    }
