736ba0831cba9ad6eece907b79c068bdf429808a
Utsav Raj <utsavrajdocx@gmail.com>
2026-07-05 13:25:50 +0530

    Dp Practice

--- 2026_Q130/DynamicPrograming/CoinChange.cpp
+++ 2026_Q130/DynamicPrograming/CoinChange.cpp
@@ -4,23 +4,18 @@
 using namespace std;
 
     int coinChange(vector<int>& coins, int amount) {
-        
-        vector<int> amt(amount + 1, amount + 1);
-
-        amt[0] = 0;
-
-        for (int i = 1; i <= amount; i++) {
-            for (int j = 0; j < coins.size(); j++) {
-                if (i >= coins[j]) {
-                    amt[i] = min(amt[i], 1 + amt[i - coins[j]]);
+        vector<int>amt(amount+1,amount+1);
+        amt[0]=0;
+        for(int i=1;i<=amount;i++){
+            for(int j=0;j<=coins.size();j++){
+                if(i>=coins[j]){
+                    amt[i] = min(amt[i],1+amt[i-coins[j]]);
                 }
             }
         }
-
-        if (amt[amount] < amount + 1) {
+        if(amt[amount]<amount+1){
             return amt[amount];
         }
-
         return -1;
     }
 
