f9704df78abef08a87441e5cf4bf41349f61280e
Utsav Raj <utsavrajdocx@gmail.com>
2026-07-05 18:44:46 +0530

    DP Practice

--- 2026_Q130/DynamicPrograming/WordBreak.cpp
+++ 2026_Q130/DynamicPrograming/WordBreak.cpp
@@ -4,25 +4,17 @@
 using namespace std;
 
 bool wordBreak(string s, vector<string>& wordDict) {
-
-    vector<bool> dp(s.length() + 1, false);
-    dp[0] = true;
-
-    unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
-
-    for (int i = 1; i <= s.length(); i++) {
-
-        for (int j = i - 1; j >= 0; j--) {
-
-            if (dp[j] &&
-                wordSet.count(s.substr(j, i - j))) {
-
+    vector<bool>dp(s.length()+1,false);
+    dp[0]=true;
+    unordered_set<string>wordSet(wordDict.begin(),wordDict.end());
+    for(int i=1;i<=s.length();i++){
+        for(int j=i;j>=0;j--){
+            if(dp[j] && wordSet.count(s.substr(j,i-j))){
                 dp[i] = true;
                 break;
             }
         }
     }
-
     return dp[s.length()];
 }
 
