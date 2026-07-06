f09da37d2984b30a4718bc99bcc8a8c3f9011cfa
Utsav Raj <utsavrajdocx@gmail.com>
2026-07-06 09:21:37 +0530

    Max ele

--- 2026_Q130/Sorting/MajorityElement_hashMap.cpp
+++ 2026_Q130/Sorting/MajorityElement_hashMap.cpp
@@ -4,25 +4,21 @@
 using namespace std;
 
 int mostFrequent(vector<int>& nums) {
-    unordered_map<int, int> mp;
-
-    int maxFreq = 0;
+unordered_map<int,int>mp;
+    int maxi=0;
     int ans = nums[0];
-
-    for (int num : nums) {
+    for(int num:nums){
         mp[num]++;
-
-        if (mp[num] > maxFreq) {
-            maxFreq = mp[num];
+        if(mp[num]>maxi){
+            maxi = mp[num];
             ans = num;
         }
     }
-
     return ans;
 }
 
 int main() {
-    vector<int> v = {1,2,3,4,5,6,7,8,9,1};
+    vector<int> v = {1,1,1,1,1,1,0,0,2,3,1,5,6,7,8,9,5,4,1,2,3,6,0,5,6,5,8,8,5};
 
     cout << mostFrequent(v);
 }
