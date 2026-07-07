3594d100dd45fd855ed87d7bd563c39f9bd51ca4
Utsav Raj <utsavrajdocx@gmail.com>
2026-07-07 18:54:27 +0530

    Leet

---
+++ DSA.CPP/Graph/DFS.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <queue>
+using namespace std;
+
+class Graph{
+    int V;
+    list<int>* l;
+    public:
+    Graph(int V){
+        this->V=V;
+        l = new list<int>[V];
+    }
+    void addEdge(int u,int v){
+        l[u].push_back(v);
+        l[v].push_back(u);
+    }
+    void dfsHelper(int u,vector<bool>& vis){
+        cout<<u<<" ";
+        vis[u]=true;
+        for(int v:l[u]){
+            if(!vis[v]){
+                dfsHelper(v,vis);
+            }
+        }
+    }
+    void dfs(){
+        int src = 0;
+        vector<bool>vis(V,false);
+        dfsHelper(src,vis);
+    }
+};
+
+
+int main(){
+    Graph g(5);
+    g.addEdge(0,1);
+    g.addEdge(1,2);
+    g.addEdge(1,3);
+    g.addEdge(2,4);
+
+    g.dfs();
+}
