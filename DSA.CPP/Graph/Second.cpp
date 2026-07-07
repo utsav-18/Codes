6daeba6e8630c11f307b8457f460b459455490e1
Utsav Raj <utsavrajdocx@gmail.com>
2026-07-07 11:21:43 +0530

    Graph

--- DSA.CPP/Graph/Second.cpp
+++ DSA.CPP/Graph/Second.cpp
@@ -5,40 +5,34 @@ using namespace std;
 
 class Graph{
     int V;
-    list<int>*l;
-public:
+    list<int>* l;
+    public:
     Graph(int V){
         this->V=V;
         l = new list<int>[V];
     }
-
     void addEdge(int u,int v){
         l[u].push_back(v);
         l[v].push_back(u);
     }
-
-    void printAdjList(){
+    void print(){
         for(int i=0;i<V;i++){
-            cout<<i<<" : ";
-            for(int neigh:l[i]){
-                cout<<neigh<<" ";
-            }
-            cout<<endl;
+            cout<<i<<": ";
+            for(int n: l[i]){
+                cout<<n<<" ";
+            }cout<<endl;
         }
     }
-    
 };
 
-int main(){
 
+int main(){
     Graph g(5);
     g.addEdge(0,1);
     g.addEdge(1,2);
     g.addEdge(1,3);
     g.addEdge(2,3);
     g.addEdge(2,4);
-    
-    g.printAdjList();
 
-    return 0;
+    g.print();
 }
