6e2137612cc14115986f56c6229a58044f29d1bd
Utsav Raj <utsavrajdocx@gmail.com>
2026-07-10 12:29:24 +0530

    Candies

---
+++ 2026_Q130/Array/candy.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std; 
+
+int candy(vector<int>& ratings) {
+    int n = ratings.size();
+    vector<int>candies(n,1);
+    for(int i=1;i<n;i++){
+        if(ratings[i]>ratings[i-1]){
+            candies[i] = candies[i-1] + 1;
+        }
+    }
+
+    for(int i=2;i>=0;i--){
+        if(ratings[i]>ratings[i+1]){
+            candies[i] = max(candies[i],candies[i+1]+1);
+        }
+    }
+
+    int total = 0;
+    for(int c : candies){
+        total+=c;
+    }
+
+    return total;
+
+}
+
+int main(){
+    vector<int>v;
+    v={1,2,2};
+    cout<<candy(v)<<" Candies are needed!!";
+
+}
