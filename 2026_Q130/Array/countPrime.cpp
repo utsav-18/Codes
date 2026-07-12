f246a99b3ee7a059e9fe789d6a98f5b8515c8fe0
Utsav Raj <utsavrajdocx@gmail.com>
2026-07-12 19:35:12 +0530

    Graph

---
+++ 2026_Q130/Array/countPrime.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+    int countPrimes(int n) {
+        int c = 0;
+
+        for(int i=2;i<n;i++){
+            int j=2;
+            bool flag = true;
+
+            while(j*j<=i){
+                if(i%j==0){
+                    flag = false;
+                    break;
+                }
+                j++;
+            }
+
+            if(flag){
+                c++;
+            }
+        }
+
+        return c;
+    }
+
+    int main(){
+        cout<<countPrimes(10);
+    }
