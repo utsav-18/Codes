7d19b9349ba1f4bf7098dd74e7d0e7128c4a2a1b
Utsav Raj <utsavrajdocx@gmail.com>
2026-07-13 17:30:38 +0530

    Count prime

--- 2026_Q130/Array/countPrime.cpp
+++ 2026_Q130/Array/countPrime.cpp
@@ -1,28 +1,55 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-    int countPrimes(int n) {
-        int c = 0;
+    // int countPrimes(int n) {
+    //     int c = 0;
 
-        for(int i=2;i<n;i++){
-            int j=2;
-            bool flag = true;
+    //     for(int i=2;i<n;i++){
+    //         int j=2;
+    //         bool flag = true;
 
-            while(j*j<=i){
-                if(i%j==0){
-                    flag = false;
-                    break;
-                }
-                j++;
+    //         while(j*j<=i){
+    //             if(i%j==0){
+    //                 flag = false;
+    //                 break;
+    //             }
+    //             j++;
+    //         }
+
+    //         if(flag){
+    //             c++;
+    //         }
+    //     }
+
+    //     return c;
+    // }
+
+   int countPrimes(int n) {
+        vector<bool>prime(n,true);
+            if(n<2){
+                return 0;
             }
+        prime[0] = false;
+        prime[1] = false;
 
-            if(flag){
-                c++;
+        for(int i=2;i*i<n;i++){
+            if(prime[i]){
+                for(int j=i*i;j<n;j=j+i){
+                    prime[j]=false;
+                }
             }
         }
 
+        int c=0;
+        for(int i=0;i<n;i++){
+            if(prime[i]){
+                c++;
+            }
+        }
         return c;
     }
+
 
     int main(){
         cout<<countPrimes(100);
