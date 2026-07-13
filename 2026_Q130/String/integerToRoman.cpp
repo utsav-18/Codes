1e28c58c328c62a6a3f06169a7115d3d5b41bf05
Utsav Raj <utsavrajdocx@gmail.com>
2026-07-13 17:52:13 +0530

    Leet

---
+++ 2026_Q130/String/integerToRoman.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+    string intToRoman(int num) {
+        string ans = "";
+
+        while (num > 0) {
+            if (num >= 1000) {
+                num -= 1000;
+                ans.push_back('M');
+            }
+            else if (num >= 900) {
+                num -= 900;
+                ans += "CM";
+            }
+            else if (num >= 500) {
+                num -= 500;
+                ans.push_back('D');
+            }
+            else if (num >= 400) {
+                num -= 400;
+                ans += "CD";
+            }
+            else if (num >= 100) {
+                num -= 100;
+                ans.push_back('C');
+            }
+            else if (num >= 90) {
+                num -= 90;
+                ans += "XC";
+            }
+            else if (num >= 50) {
+                num -= 50;
+                ans.push_back('L');
+            }
+            else if (num >= 40) {
+                num -= 40;
+                ans += "XL";
+            }
+            else if (num >= 10) {
+                num -= 10;
+                ans.push_back('X');
+            }
+            else if (num >= 9) {
+                num -= 9;
+                ans += "IX";
+            }
+            else if (num >= 5) {
+                num -= 5;
+                ans.push_back('V');
+            }
+            else if (num >= 4) {
+                num -= 4;
+                ans += "IV";
+            }
+            else if (num >= 1) {
+                num -= 1;
+                ans.push_back('I');
+            }
+        }
+
+        return ans;
+    }
+
+    int main(){
+        int num;
+        cout<<"Enter a number: ";
+        cin>>num;
+        cout<<intToRoman(num);
+    }
