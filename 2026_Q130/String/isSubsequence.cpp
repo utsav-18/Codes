f491e21e740a1bbf61b1a3b2538c356965801a62
Utsav Raj <utsavrajdocx@gmail.com>
2026-07-15 16:02:28 +0530

    Leet

--- 2026_Q130/String/isSubsequence.cpp
+++ 2026_Q130/String/isSubsequence.cpp
@@ -2,26 +2,16 @@
 using namespace std;
 
 bool isSubsequence(string s, string t) {
-    int s1 = s.length();
-    int s2 = t.length();
-    int idx = 0;
-
-    for(int i=0;i<s1;i++){
-        bool flag = false;
-        for(int j = idx;j<s2;j++){
-            if(s[i]==t[j]){
-                flag = true;
-                idx = j+1;
-                break;
-            }
+    int i=0;
+    int j=0;
+    while(i<s.length() && j<t.length()){
+        if(s[i]==t[j]){
+            i++;
         }
-        if(!flag){
-            return false;
-        }
-
+        j++;
     }
 
-    return true;
+    return i==s.length();
 
 }
 
