d14fa4d96e2f499ac00213b659af515c3af3c685
Utsav Raj <utsavrajdocx@gmail.com>
2026-07-19 13:03:04 +0530

    HashMap

---
+++ 2026_Q130/HashMap/isomorphic-strings.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+bool isIsomorphic(string s, string t) {
+    unordered_map<char,char>mp1;
+    unordered_map<char,char>mp2;
+    for(int i=0;i<s.length();i++){
+        if(mp1.count(s[i])){
+            if(mp1[s[i]]!=t[i]){
+                return false;
+            }
+        }
+        else{
+            mp1[s[i]]=t[i];
+        }
+        if(mp2.count(t[i])){
+            if(mp2[t[i]]!=s[i]){
+                return false;
+            }
+        }
+        else{
+            mp2[t[i]]=s[i];
+        }
+    }
+    return true;
+}
+
+int main(){
+    string a = "egg";
+    string b = "add";
+    cout<<boolalpha;
+    cout<<isIsomorphic(a,b);
+}
+
