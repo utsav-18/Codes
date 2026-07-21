be8ae76cdda137fda321a125abf1211061cff05c
Utsav Raj <utsavrajdocx@gmail.com>
2026-07-21 17:22:15 +0530

    Practice

---
+++ ApnaCollegeDSA/Graph/DijkstraPractice.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <climits>
+using namespace std;
+
+class Edge{
+public:
+    int v;
+    int wt;
+    Edge(int v,int wt){
+        this->v=v;
+        this->wt=wt;
+    }
+};
+
+void dijkstra(int src,vector<vector<Edge>>& g,int V){
+    vector<int>dist(V,INT_MAX);
+    dist[src]=0;
+
+    priority_queue<
+        pair<int,int>,
+        vector<pair<int,int>>,
+        greater<pair<int,int>>
+    >pq;
+
+    pq.push({0,src});
+
+    while(pq.size()>0){
+        int u = pq.top().second;
+        pq.pop();
+        for(Edge e:g[u]){
+            if(dist[e.v]>dist[u] + e.wt){
+                dist[e.v] = dist[u] + e.wt;
+                pq.push({dist[e.v],e.v});
+            }
+        }
+    }
+
+    for(int i=0;i<dist.size();i++){
+        cout<<dist[i]<<" ";
+    }
+
+}
+
+
+int main(){
+    int V = 6;
+    vector<vector<Edge>> g(V);
+
+    g[0].push_back(Edge(1,2));
+    g[0].push_back(Edge(2,4));
+
+    g[1].push_back(Edge(2,1));
+    g[1].push_back(Edge(3,7));
+
+    g[2].push_back(Edge(4,3));
+
+    g[3].push_back(Edge(5,1));
+
+    g[4].push_back(Edge(3,2));
+    g[4].push_back(Edge(5,5));
+
+    dijkstra(0,g,V);
+
+}
