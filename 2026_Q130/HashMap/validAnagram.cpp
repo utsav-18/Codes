a513bf52774534ca5eed16f6eeade8a455ebf6e2
Utsav Raj <utsavrajdocx@gmail.com>
2026-07-22 11:10:59 +0530

    hash

---
+++ 2026_Q130/HashMap/validAnagram.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+bool isAnagram(string s, string t) {
+
+    if(s.length()!=t.length()){
+        return false;
+    }
+
+    unordered_map<char,int> mp;
+
+    for(char c:s){
+        mp[c]++;
+    }
+
+    for(char c:t){
+        if(mp.count(c)==0){
+            return false;
+        }
+        else{
+            mp[c]--;
+        }
+        if(mp[c]==0){
+            mp.erase(c);
+        }
+    }
+
+    return true;
+}
+
+int main(){
+string a = "anagram";
+string b = "nagaram";
+cout<<boolalpha;
+cout<<isAnagram(a,b);
+}
