3c9e2d71b17b61b7b87374b953a612147b9595e3
Utsav Raj <utsavrajdocx@gmail.com>
2026-07-22 12:01:35 +0530

    hash

---
+++ 2026_Q130/HashMap/happySum.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
+using namespace std;
+
+int squareSum(int n){
+    int sum = 0;
+    while(n>0){
+        int digit = n%10;
+        sum += digit*digit;
+        n /= 10;
+    }
+    return sum;
+}
+
+bool isHappy(int n) {
+    unordered_set<int> seen;
+    while(n!=1){
+        if(seen.count(n)) return false;
+        seen.insert(n);
+        n = squareSum(n);
+    }
+    return true;
+}
+
+int main(){
+    int n = 13;
+    cout<<boolalpha;
+    cout<<isHappy(n);
+}
