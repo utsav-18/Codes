ce3e36952d8ee41ab9575d4dd744ad95c02c434d
Utsav Raj <utsavrajdocx@gmail.com>
2026-07-27 18:15:35 +0530

    Kruskal

---
+++ ApnaCollegeDSA/Graph/KruskalsAlgorithm.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Edge{
+public:
+    int u,v,wt;
+    Edge(int u,int v,int wt){
+        this->u=u;
+        this->v=v;
+        this->wt=wt;
+    }
+
+    //comparator => <
+    bool operator<(const Edge &other) const{
+        return this->wt < other.wt;
+    }
+};
+
+class Graph{
+public:     
+    int V;
+    vector<Edge>edges;
+    vector<int>par,rank;
+        Graph(int V){
+            this->V=V;
+            for(int i=0;i<V;i++){
+                par.push_back(i);
+                rank.push_back(0);
+            }
+        }
+        void addEdge(int u, int v, int wt){
+            edges.push_back(Edge(u,v,wt));
+        }
+
+        int find(int x){
+            if(par[x]==x){
+                return x;
+            }
+            return par[x] = find(par[x]);
+        }
+
+        void unionByRank(int a,int b){
+            int parA = find(a);
+            int parB = find(b);
+
+            if(parA==parB) return;
+            if(rank[parA] == rank[parB]){
+                par[parB] = parA;
+                rank[parA]++;
+            }
+            else if(rank[parA]>rank[parB]){
+                par[parB] = parA;
+            }
+            else{
+                par[parA] = parB;
+            }
+        }
+
+        void kruskal(){
+            sort(edges.begin(),edges.end());
+            int mstCost = 0;
+            int count = 0;
+            for(int i=0;i<edges.size();i++){
+                Edge e = edges[i];
+                int paru = find(e.u);
+                int parv = find(e.v);
+                    if(paru!=parv){
+                        unionByRank(e.u,e.v);
+                        mstCost+=e.wt;
+                        count++;
+                    }
+            }
+            cout<<"Mst Cost: "<<mstCost<<endl;
+        }
+};
+
+int main(){
+    Graph graph(4);
+    
+    graph.addEdge(0,1,10);
+    graph.addEdge(0,2,6);
+    graph.addEdge(0,3,5);
+    graph.addEdge(1,3,15);
+    graph.addEdge(2,3,4);
+
+    graph.kruskal();
+
+}
