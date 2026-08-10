8c00cfd5509debb391f764bc6034c110c4db7fd6
Utsav Raj <utsavrajdocx@gmail.com>
2026-08-10 18:26:41 +0530

    Tree

--- 2026_IMP/Tree/InvertTree.cpp
+++ 2026_IMP/Tree/InvertTree.cpp
@@ -35,6 +35,21 @@ TreeNode* invertTree(TreeNode* root) {
 
         return root;
     }
+
 int main(){
 
+    TreeNode* root = new TreeNode(4);
+
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(7);
+
+    root->left->left = new TreeNode(1);
+    root->left->right = new TreeNode(3);
+
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(9);
+
+    cout << "Before: ";
+    inorder(root);
+    
 }
