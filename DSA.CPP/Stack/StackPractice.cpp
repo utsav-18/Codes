997b4745ae308aeb4daa497535b42f2242f8810d
Utsav Raj <utsavrajdocx@gmail.com>
2026-08-11 22:20:03 +0530

    Stack

--- DSA.CPP/Stack/StackPractice.cpp
+++ DSA.CPP/Stack/StackPractice.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main(){
     stack<int>st;
-    st.push(10);
-    st.push(20);
-    st.push(30);
+    st.push(100);
+    st.push(200);
+    st.push(300);
 
     while(!st.empty()){
         cout<<st.top()<<" ";
