b91305214fc827a1262de21d7eaa3b637f2e6403
Utsav Raj <utsavrajdocx@gmail.com>
2026-08-13 12:30:58 +0530

    Stack

--- 2026_IMP/Stack/minStack.cpp
+++ 2026_IMP/Stack/minStack.cpp
@@ -2,14 +2,18 @@
 #include <vector>
 using namespace std;
 
+class MinStack {
+public:
+
     vector<int> st;
     vector<int> minSt;
 
     MinStack() {
-        
+
     }
-    
+
     void push(int val) {
+
         st.push_back(val);
 
         if(minSt.empty()) {
@@ -19,21 +23,41 @@ using namespace std;
             minSt.push_back(min(val, minSt.back()));
         }
     }
-    
+
     void pop() {
+
         st.pop_back();
         minSt.pop_back();
     }
-    
+
     int top() {
+
         return st.back();
     }
-    
+
     int getMin() {
+
         return minSt.back();
     }
+};
 
+int main() {
 
-int main(){
+    MinStack s;
 
+    s.push(5);
+    s.push(3);
+    s.push(7);
+    s.push(2);
+
+    cout << "Top: " << s.top() << endl;
+    cout << "Minimum: " << s.getMin() << endl;
+
+    s.pop();
+
+    cout << "After pop:" << endl;
+    cout << "Top: " << s.top() << endl;
+    cout << "Minimum: " << s.getMin() << endl;
+
+    return 0;
 }
