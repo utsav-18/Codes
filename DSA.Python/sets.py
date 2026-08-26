8b305d1246e3f2db220b21d2671a14cf708b1f4b
Utsav Raj <utsavrajdocx@gmail.com>
2026-08-26 19:38:39 +0530

    py

--- DSA.Python/sets.py
+++ DSA.Python/sets.py
@@ -1,12 +1,19 @@
 # nums = {1,2,2,3,3,4,4,5,5,6}
 # print(nums)
 
-arr = [1,2,3,2,4,5]
+# arr = [1,2,3,2,4,5]
 
-seen = set()
+# seen = set()
 
-for x in arr:
-    if x in seen:
-        print("Duplicate: ",x)
-        break
-    seen.add(x)
+# for x in arr:
+#     if x in seen:
+#         print("Duplicate: ",x)
+#         break
+#     seen.add(x)
+
+# a = {1,2,3,4,5}
+# b = {3,4,5,6}
+
+# print(a.union(b))
+# print(a.intersection(b))
+# print(a-b)
