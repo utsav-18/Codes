02d80fcd5b344f3e280488ac3693af471ea4a173
Utsav Raj <utsavrajdocx@gmail.com>
2026-08-26 12:15:27 +0530

    py

---
+++ DSA.Python/basics.py
@@ -0,0 +1,10 @@
+# n = int(input("Enter a  number: "))
+# print("This is the number: ",n)
+
+# name = input("Enter your name: ")
+# print(name)
+
+a,b = map(int,input().split())
+
+print(a,end=" ")
+print(b)
