ff3287425d4ef6e40aa8d4f2573d0439f38e5896
Utsav Raj <utsavrajdocx@gmail.com>
2026-08-26 19:19:28 +0530

    py

--- DSA.Python/Tuples_basics.py
+++ DSA.Python/Tuples_basics.py
@@ -1,3 +1,8 @@
-person = ("Utsav",21,"B.Tech")
-name, age, degree = person
-print(name,age,degree)
+# person = ("Utsav",21,"B.Tech")
+# name, age, degree = person
+# print(name,age,degree)
+
+t = (1,2,3,4,5,1,2)
+#print(t.count(2))
+
+print(t.index(3))
