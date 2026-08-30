9b1a216727df966a1c3b09a34fc193c6bed3f30a
Utsav Raj <utsavrajdocx@gmail.com>
2026-08-30 18:48:48 +0530

    Vect

--- C++/Vectors/buildINreverce.cpp
+++ C++/Vectors/buildINreverce.cpp
@@ -8,11 +8,11 @@
 
         vector<int>v;
 
-        v.push_back(10);
         v.push_back(0);
         v.push_back(1);
+        v.push_back(2);
+        v.push_back(3);
         v.push_back(4);
-        v.push_back(5);
 
    sort(v.begin(),v.end());
 
