511b6f23ce7526656ea85f3f92b11d8085406154
Utsav Raj <utsavrajdocx@gmail.com>
2026-08-30 18:47:19 +0530

    Vect

--- C++/Vectors/basic.cpp
+++ C++/Vectors/basic.cpp
@@ -6,10 +6,10 @@
 
             vector<int>v;  //no need to mention size;
                v.push_back(0);
-                v.push_back(1);
-                 v.push_back(2);
-                   v.push_back(3);
-                    v.push_back(4);
+                v.push_back(10);
+                 v.push_back(20);
+                   v.push_back(30);
+                    v.push_back(40);
 
             cout<<v[0]<<" ";
              cout<<v[1]<<" ";
