dce7e2991ddba6fe1b493b07973d62bca594ad88
Utsav Raj <utsavrajdocx@gmail.com>
2026-08-31 12:41:52 +0530

    Py

--- DSA.Python/basic.py
+++ DSA.Python/basic.py
@@ -6,3 +6,13 @@ print(list)
 print("Set: ",end="")
 set = {1,2,3,4,5,5,6,7,7}
 print(set)
+
+
+student={
+    "Utsav" : 21,
+    "Aman" : 21,
+    "Ram" : 20
+}
+
+for key,value in student.items():
+    print(key,":",value)
