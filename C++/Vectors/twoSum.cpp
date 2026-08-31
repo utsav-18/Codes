2a0c67221ff4c6dcabbe40c1d7c8fd6e822d83de
Utsav Raj <utsavrajdocx@gmail.com>
2026-08-31 12:31:25 +0530

    Vect

--- C++/Vectors/twoSum.cpp
+++ C++/Vectors/twoSum.cpp
@@ -1,44 +1,36 @@
 #include <iostream>
 #include <vector>
 
-    using namespace std;
+using namespace std;
 
-    int main (){
+int main() {
 
-        // int arr[5]={1,2,3,4,5};
+    int x, s, e;
 
+    cout << "Enter array size: ";
+    cin >> s;
 
-        // for(int i=0;i<5;i++){
-        //    for(int j=i+1;j<4;j++){
+    vector<int> v;
 
-        //     if(arr[i]+arr[j]==3) //printf("%d %d\n",arr[i],arr[j]);
-        //                 cout<<arr[i]<<" "<<arr[j]<<"\n";
-        //    }
-        // }
-
-        int x,s,e;
-
-        cout<<"Enter array size:";
-        cin>>s;
-
-        vector<int>v;
-
-        for(int i=0;i<s;i++){
-            cout<<"Enter element "<<i<<":";
-            cin>>e;
-            v.push_back(e);
-        }
-
-        cout<<"Enter target:";
-        cin>>x;
-
-        for(int i=0;i<5;i++){
-           for(int j=i+1;j<4;j++){
-
-            if(v.at(i)+v.at(j)==x) printf("%d %d\n",v.at(i),v.at(j));
-
-           }
-        }
-
-
+    for (int i = 0; i < s; i++) {
+        cout << "Enter element " << i << ": ";
+        cin >> e;
+        v.push_back(e);
     }
+
+    cout << "Enter target: ";
+    cin >> x;
+
+    for (int i = 0; i < s; i++) {
+
+        for (int j = i + 1; j < s; j++) {
+
+            if (v.at(i) + v.at(j) == x) {
+                cout << v.at(i) << " " << v.at(j) << endl;
+            }
+
+        }
+    }
+
+    return 0;
+}
