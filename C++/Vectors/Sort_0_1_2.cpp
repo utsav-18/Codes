6b3b28e5a03ae5137cb8c86f5cf4c1e287121401
Utsav Raj <utsavrajdocx@gmail.com>
2026-08-31 12:30:08 +0530

    Vect

--- C++/Vectors/Sort_0_1_2.cpp
+++ C++/Vectors/Sort_0_1_2.cpp
@@ -1,49 +1,50 @@
-//writen by me;
-
+// Written by me
 
 #include <iostream>
 #include <vector>
 
-    using namespace std;
-    int main () {
+using namespace std;
 
-        vector<int>v;
-        v.push_back(1);
-        v.push_back(0);
-        v.push_back(1);
-        v.push_back(0);
-        v.push_back(0);
-        v.push_back(1);
-        v.push_back(1);
-        v.push_back(0);
+int main() {
 
-int c=0;
+    vector<int> v;
 
-cout<<"Original: ";
-for(int i=0;i<v.size();i++){
-    cout<<v[i]<<" ";
+    v.push_back(1);
+    v.push_back(0);
+    v.push_back(1);
+    v.push_back(0);
+    v.push_back(0);
+    v.push_back(1);
+    v.push_back(1);
+    v.push_back(0);
 
-}   cout<<"\n";
+    int c = 0;
 
+    cout << "Original: ";
 
-for(int i=0;i<v.size();i++){
-
-    if(v[i]==0) {
-
-        int temp=v[i];
-        v[i]=v[c];
-        v[c]=temp;
-        c++;
-
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << " ";
     }
 
+    cout << "\n";
+
+    // Sorting 0s and 1s
+    for (int i = 0; i < v.size(); i++) {
+
+        if (v[i] == 0) {
+
+            swap(v[i], v[c]);
+            c++;
+        }
+    }
+
+    cout << "Sorted:   ";
+
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << " ";
+    }
+
+    cout << "\n";
+
+    return 0;
 }
-
-cout<<"Sorted:   ";
-for(int i=0;i<v.size();i++){
-    cout<<v[i]<<" ";
-
-}   cout<<"\n";
-
-
-    }
