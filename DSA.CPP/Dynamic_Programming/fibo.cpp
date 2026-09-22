4bd92bb757335d8578fcc0690b74a5e9339933bc
Utsav Raj <utsavrajdocx@gmail.com>
2026-09-22 21:04:26 +0530

    DP

---
+++ DSA.CPP/Dynamic_Programming/fibo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 6;
+
+    int f[n+1];
+    f[0]=0;
+    f[1]=1;
+
+
+    for(int i=2;i<=n;i++){
+        f[i] = f[i-1] + f[i-2];
+    }
+
+    cout<<f[n];
+}
