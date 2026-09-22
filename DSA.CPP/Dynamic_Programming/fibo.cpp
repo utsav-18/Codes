675e430120b00d60f16ccd9fef4a2e17b3f9a055
Utsav Raj <utsavrajdocx@gmail.com>
2026-09-22 21:10:03 +0530

    DP

--- DSA.CPP/Dynamic_Programming/fibo.cpp
+++ DSA.CPP/Dynamic_Programming/fibo.cpp
@@ -1,17 +1,12 @@
 #include <iostream>
 using namespace std;
 
+int fibonacci(int n){
+    if(n<=1) return n;
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
 int main(){
     int n = 6;
-
-    int f[n+1];
-    f[0]=0;
-    f[1]=1;
-
-
-    for(int i=2;i<=n;i++){
-        f[i] = f[i-1] + f[i-2];
-    }
-
-    cout<<f[n];
+    cout<<fibonacci(n);
 }
