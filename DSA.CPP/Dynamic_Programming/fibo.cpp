33f119af7b719df6b0907dfd7538a31076e0ce37
Utsav Raj <utsavrajdocx@gmail.com>
2026-09-22 21:13:56 +0530

    DP

--- DSA.CPP/Dynamic_Programming/fibo.cpp
+++ DSA.CPP/Dynamic_Programming/fibo.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int fibonacci(int n){
+// int fibonacci(int n){
+//     if(n<=1) return n;
+//     return fibonacci(n-1) + fibonacci(n-2);
+// }
+
+int fibonacci(int n,vector<int>&v){
     if(n<=1) return n;
-    return fibonacci(n-1) + fibonacci(n-2);
+    if(v[n]!=-1) return v[n];
+    return v[n] = fibonacci(n-1,v) + fibonacci(n-2,v);
 }
 
 int main(){
     int n = 6;
-    cout<<fibonacci(n);
+     vector<int> v(n+1,-1);
+    cout<<fibonacci(n,v);
 }
